4486151a8e94264781d98d66c2b47b093026892e
rashedabdullah <maarashed40@gmail.com>
2023-11-27 18:31:37 +0600

    custom hook and useRef

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Showdata from './Components/Showdata';
+import Time from './Components/Time';
+function App() {
+
+  return (
+    <div>
+      <Time />
+      <Showdata />
+    </div>
+  )
+}
+
+export default App;
